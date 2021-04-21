a0bd828857ad9add0047ca981f3858b6496cc62b
David Ruiz Tornés <davidruiz1996@gmail.com>
2021-04-21 02:44:13 +0200

    Canvis adapters login

--- login/adapters.py
+++ login/adapters.py
@@ -7,29 +7,30 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from allauth.account.signals import user_signed_up
 from django.contrib.auth import get_user_model
+from django.shortcuts import resolve_url
 
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
 
     def pre_social_login(self, request, sociallogin):
         email = sociallogin.user.email.lower().split("@")
-        user = User.objects.get(email=sociallogin.user.email)
         if email[1] != "sapalomera.cat":
             raise ImmediateHttpResponse(render(request, "accounts/login.html"))
-        if user.groups.all() != 0:
-            print("hola")
 
 
 class MyAccountAdapter(DefaultAccountAdapter):
+
+    def get_signup_redirect_url(self, request):
+        print(self)
+        print(request)
+        return resolve_url("/grups/")
+
     def get_login_redirect_url(self, request):
         email = request.user.email
         user = User.objects.get(email=email)
         grups = user.groups.all()
-        print(grups)
-        print(user)
-        print(email)
         if not grups:
-            path = "/groups/"
+            path = "/grups/"
             return path
         else:
             path = "/aplicacions/"
