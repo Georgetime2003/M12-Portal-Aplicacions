84854a0b53f84ece94f07c92620084177b3fdb87
David Ruiz Tornés <davidruiztornes1996@gmail.com>
2021-05-26 01:57:40 +0200

    Update settings.py

--- portal_aplicacions/settings.py
+++ portal_aplicacions/settings.py
@@ -89,10 +89,10 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+#import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
