062073336d59be6525a327a93781aec876d183fc
David Ruiz Tornés <davidruiztornes1996@gmail.com>
2021-05-26 16:20:35 +0200

    canvis deploy

--- autenticacio/admin.py
+++ autenticacio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Curs,Group
+from .models import Curs,Group,Rol
 from django.contrib import admin
 from allauth.socialaccount.models import SocialAccount, SocialToken
 from allauth.account.models import EmailAddress
@@ -7,6 +7,7 @@ from allauth.account.models import EmailAddress
 
 admin.site.register(Curs)
 admin.site.register(Group)
+admin.site.register(Rol)
 
 admin.site.unregister(SocialToken)
 admin.site.unregister(SocialAccount)
